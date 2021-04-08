52ae3215ba5d7fc9b1cfee1207c9e30e07b2feef
sahanepic <sahanepic@gmail.com>
2021-04-08 11:28:25 +0530

    first commit

---
+++ src/main/java/com/sahanbcs/main/Main.java
@@ -0,0 +1,10 @@
+package com.sahanbcs.main;
+
+import com.sahanbcs.utills.Printer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer.print("Hello SAhan");
+    }
+}
---
+++ src/main/java/com/sahanbcs/utills/Printer.java
@@ -0,0 +1,16 @@
+package com.sahanbcs.utills;
+
+ 
+import java.util.logging.Logger;
+
+public class Printer {
+
+    private Printer(){
+
+    }
+
+    public static void print(String str){
+        Logger logger = Logger.getLogger(Printer.class.getName() );
+        logger.info(str);
+    }
+}
